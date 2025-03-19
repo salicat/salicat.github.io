3b54f11ca65b9846063fd714b91ab3e157e169a1
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2025-03-19 22:46:11 +0100

    estilos globales

--- src/App.vue
+++ src/App.vue
@@ -11,28 +11,28 @@ const toggleSidebar = () => {
 </script>
 
 <template> 
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="Carlos Cortes Sanchez" />
-      <br>
-      <!-- Botón para mostrar/ocultar el sidebar -->
-      <button @click="toggleSidebar" class="sidebar-toggle-btn">
-        <span v-if="isSidebarVisible">Cerrar</span>
-        <span v-else>Mostrar</span>
-      </button>
+	<header>
+		<div class="wrapper">
+		<HelloWorld msg="Carlos Cortes Sanchez" />
+		<br>
+		<!-- Botón para mostrar/ocultar el sidebar -->
+		<button @click="toggleSidebar" class="sidebar-toggle-btn">
+			<span v-if="isSidebarVisible">Cerrar</span>
+			<span v-else>Mostrar</span>
+		</button>
 
-      <nav :class="{ hidden: !isSidebarVisible }">
-        <RouterLink to="/skills">Skills</RouterLink>
-        <RouterLink to="/experience">Job Experience</RouterLink>
-        <RouterLink to="/projects">Proyectos</RouterLink>
-        <RouterLink to="/cv">C.V.</RouterLink>
-      </nav>
-    </div>
-  </header>
+		<nav :class="{ hidden: !isSidebarVisible }">
+			<RouterLink to="/skills">Skills</RouterLink>
+			<RouterLink to="/experience">Job Experience</RouterLink>
+			<RouterLink to="/projects">Proyectos</RouterLink>
+			<RouterLink to="/cv">C.V.</RouterLink>
+		</nav>
+		</div>
+	</header>
 
-  <div :class="{ 'router-content': true, 'expanded': isSidebarVisible }">
-    <RouterView />
-  </div>
+	<div :class="{ 'router-content': true, 'expanded': isSidebarVisible }">
+		<RouterView />
+	</div>
 </template>
 
 
