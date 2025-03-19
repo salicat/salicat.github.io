f2835f1e424b95e81d0b8a506241636d1027d41b
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2025-03-19 21:59:39 +0100

    CSS MESS

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView 				from '../views/HomeView.vue'
 import ExperienceView 			from '../views/ExperienceView.vue'
 import ProgrammingSkillsView 	from "../views/ProgrammingSkillsView.vue"
 import CVView					from '@/views/CVView.vue'
+import ProjectsView 			from '../views/ProjectsView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,9 +14,9 @@ const router = createRouter({
 			component: HomeView,
 		},
 		{
-			path: '/about',
-			name: 'about',
-			component: () => import('../views/AboutView.vue'),
+			path: '/projects',
+			name: 'projects',
+			component: ProjectsView,
 		},
 		{
 			path:		'/experience',
