7bce597c5daf397acff1cdd3a5f9d379637038ee
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2025-06-10 09:03:16 +0200

    hero section on main

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,16 +1,40 @@
 <template>
-    <div class="main">
-      <h1 style="color: white;">HelloWorld</h1>
-    </div>
-    <div>
-        <p  class="module">
-            Soy un desarrollador fullstack con una sólida trayectoria en la creación y gestión de aplicaciones web integrales. 
-            Con experiencia en el desarrollo de proyectos propios, he diseñado y construido soluciones que abarcan 
-            desde la implementación de APIs con FastAPI y el desarrollo frontend con Vue.js, hasta la gestión de 
-            bases de datos en PostgreSQL. Mi enfoque polivalente me permite integrar herramientas de autenticación, 
-            despliegue en la nube y diseño digital (incluyendo WordPress, CorelDraw y Canva) para ofrecer soluciones 
-            eficientes y escalables. Destaco por mi capacidad de aprendizaje autodidacta, innovación y adaptabilidad 
-            en entornos cambiantes.
-        </p>
+    <div class="">
+        <div class="hero">
+        <img src="https://lh3.googleusercontent.com/a/ACg8ocKsuLJ-RuSHdsMXK70QntffG7izvVy4MEN0I6IsS24RHenI81o=s576-c-no" alt="Carlos Cortes" class="profile-img">
+        <h1>Carlos Cortes Sanchez</h1>
+        <h2>Desarrollador Full Stack</h2>
+        <p class="tagline">Arquitecto de soluciones digitales escalables</p>
+        <div class="switches-container">
+            <div class="switch-wrapper">
+                <label class="switch">
+                    <input type="checkbox" id="dark-mode-toggle">
+                    <span class="slider"></span>
+                </label>
+                <span>Modo Oscuro</span>
+            </div>
+            <div class="switch-wrapper">
+                <label class="switch">
+                    <input type="checkbox" id="language-toggle">
+                    <span class="slider"></span>
+                </label>
+                <span>English</span>
+            </div>
+        </div>
+        </div>
+        <div>
+            <h1 style="color: white;">Sobre Mí</h1>
+        </div>
+        <div>
+            <p  class="module">
+                Soy un desarrollador fullstack con una sólida trayectoria en la creación y gestión de aplicaciones web integrales. 
+                Con experiencia en el desarrollo de proyectos propios, he diseñado y construido soluciones que abarcan 
+                desde la implementación de APIs con FastAPI y el desarrollo frontend con Vue.js, hasta la gestión de 
+                bases de datos en PostgreSQL. Mi enfoque polivalente me permite integrar herramientas de autenticación, 
+                despliegue en la nube y diseño digital (incluyendo WordPress, CorelDraw y Canva) para ofrecer soluciones 
+                eficientes y escalables. Destaco por mi capacidad de aprendizaje autodidacta, innovación y adaptabilidad 
+                en entornos cambiantes.
+            </p>
+        </div>
     </div>
 </template>
