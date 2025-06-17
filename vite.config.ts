0919a3f10614932280c4c5df6c909387a9625ccf
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2025-06-17 21:35:43 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: './',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
   },
   build: {
-    outDir: 'dist'  // Carpeta de salida para el build
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
